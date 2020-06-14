8bd7e871a262c43d39e932d34280ded1bbb8d0eb
Stevie <xxlennygamerxx@gmail.com>
2020-06-13 19:48:22 -0700

    fargowiltassouls base

--- ModNerfReverts.cs
+++ ModNerfReverts.cs
@@ -1,8 +1,23 @@
+using On.FargowiltasSouls;
+using System;
 using Terraria.ModLoader;
 
 namespace ModNerfReverts
 {
 	public class ModNerfReverts : Mod
 	{
+		private Mod FargowiltasSouls => ModLoader.GetMod("FargowilasSouls");
+		public override void Load()
+		{
+			if (FargowiltasSouls != null)
+			{
+				On.FargowiltasSouls.FargoPlayer.ModifyWeaponDamage += RevertNerfsFargowiltasSoulsWeaponDamage;
+			}
+		}
+
+		private void RevertNerfsFargowiltasSoulsWeaponDamage(On.FargowiltasSouls.FargoPlayer.orig_ModifyWeaponDamage orig, FargowiltasSouls.FargoPlayer self, object item, ref float add, ref float mult, ref float flat)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
